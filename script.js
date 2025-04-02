6e80471a4359c39094b211aa842284cdd9dd99e5
mohamadsam9667 <146078669+mohamadsam9667@users.noreply.github.com>
2025-04-02 10:03:34 +0330

    now resive part level game

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // alert("sam");
+
+const btnRegister=document.querySelector(".register");
+const containerStart=document.querySelector(".container")
+const inputNumber=document.querySelector('input')
+
+btnRegister.addEventListener('click',function(){
+    // console.log("sam")
+    const playerCount=inputNumber.value.trim();
+
+    if (playerCount === "") {
+        alert("لطفا تعداد بازیکنان را وارد کنید");
+        return;
+    }
+
+    const number = parseInt(playerCount, 10);
+    if (isNaN(number) || number <= 0 ) {
+        alert("لطفا یک عدد معتبر وارد کنید (بزرگتر از 0)");
+        return;
+    }else{
+
+        containerStart.style.display="none"
+        
+
+    }
+
+})
