e17a03d76c478d331d18c6704004b333170995a3
mohamadsam9667 <146078669+mohamadsam9667@users.noreply.github.com>
2025-04-03 18:43:21 +0330

    add icon and add user ui

--- script.js
+++ script.js
@@ -3,6 +3,11 @@
 const btnRegister=document.querySelector(".register");
 const containerStart=document.querySelector(".container")
 const inputNumber=document.querySelector('input')
+const selectorLevel=document.querySelector('#levelSelector')
+const partAddUser=document.querySelector('.container1')
+
+
+
 
 btnRegister.addEventListener('click',function(){
     // console.log("sam")
@@ -13,15 +18,27 @@ btnRegister.addEventListener('click',function(){
         return;
     }
 
+
     const number = parseInt(playerCount, 10);
     if (isNaN(number) || number <= 0 ) {
         alert("لطفا یک عدد معتبر وارد کنید (بزرگتر از 0)");
         return;
-    }else{
+    }
+    if (number % 2 !== 0) {
+        alert("لطفا یک عدد زوج وارد کنید");
+        return;
+    }
+    
+    else{
 
         containerStart.style.display="none"
+        partAddUser.style.display='flex'
+        console.log(selectorLevel.value )
+
         
-
+        
     }
-
+    
+    
+    
 })
